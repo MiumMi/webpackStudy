2d411260c2255f8c5a3d866a1493470fb89b0b66
wuwenqian <wwq@szltech.com>
2018-08-08 17:53:26 +0800

    5、生产环境构建

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,4 @@
-const webpack = rerquire('webpack');
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
